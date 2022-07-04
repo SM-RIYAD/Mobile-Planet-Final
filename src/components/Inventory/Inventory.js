a7a9bbd6cd1505fea18f05c1e2901e1db00ee26e
SM-RIYAD <88622536+SM-RIYAD@users.noreply.github.com>
2022-07-05 01:29:14 +0600

    done

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Inventory = () => {
+    
+    return (
+        <div>
+            <h1>c191024@ugrad.iiuc.ac.bd</h1>
+            <h1>c191024@ugrad.iiuc.ac.bd</h1>
+            
+        </div>
+    );
+};
+
+export default Inventory;
